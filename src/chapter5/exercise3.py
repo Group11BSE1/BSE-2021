3b694806ec6772c73061f82099b5899a1a1eee22
group11BSE1 <ncleverpreston3056@gmail.com>
2021-04-23 08:23:24 +0300

    partially modified exercise3.py

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -1,11 +1,11 @@
 # Exercise 3
 
-#          GROUP 11 MEMBERS
+#                   GROUP 11 MEMBERS
 #       1. NIWAGABA CLEVER      2020/BSE/055/PS
 #       2. AINEBYONA ALBERT     2020/BSE/003/PS
 #       3. LEMI MANOAH JUNGO    2020/BSE/145/PS
 
-# So far from 20th/04/21- 22nd/04/21
+# So far from 20th/04/21 - 22nd/04/21
 
 # This program will simulate a simple change maker for a vending machine. It will start with
 # a stock of coins and dollars. It will then repeatedly request the price for an item to be
@@ -24,6 +24,7 @@ print("Change maker initialized.")
 
 # 1. At program start, assume a stock of 25 nickels, 25 dimes, and 25 quarters. Print the
 # contents of the stock.
+
 nickel = 25
 dime = 25
 quarter = 25
@@ -39,6 +40,7 @@ print(five, " fives")
 
 # 2. Repeatedly prompt the user for a price in the form xx.xx, where x denotes a digit, or
 # to enter ‘q’ to quit.
+
 while True:
     price = input("Enter the purchase price (xx.xx) or 'q' to quit: ")
     if price == 'q':
@@ -46,18 +48,23 @@ while True:
 
     price = float(price)
     new_price = (round(price * 100))
+
 # Checking that the price entered is a (non-negative) multiple of .05 (i.e., it is payable
 # in nickels). If not, then print an error message and start over requesting either a
 # new price or to quit (indicated by entering a ‘q’).
+
     multiple = (new_price % 5)
     if price < 0 or multiple > 0:
         print("Illegal price: Must be a non-negative multiple of 5 cents.")
         continue
 
 # Print a menu for indicating the coin/bill deposited or to cancel payment
+
     else:
-# Defining functions
-# Function for printing the menu
+
+    # Defining functions
+    # Function for printing the menu
+
         def menu():
             print("""Menu for deposits:
             'n' - deposit a nickel
@@ -66,7 +73,9 @@ while True:
             'o' - deposit a one dollar bill
             'f' - deposit a five dollar bill
             'c' - cancel the purchase""")
+
 # Function for Please take the change below
+
         def change():
             print("Please take the change below.")
             quarters = new_price // 25
@@ -77,20 +86,25 @@ while True:
             print(quarters, " quarters")
             print(dimes, " dimes")
             print(nickles, " nickles")
+
 # Function for deposit and payment due
+
         def deposit():
             due_pay = int(price // 1)
-            due_pay1 = float(price % 1)
+            due_pay1 = round(float(price % 1) * 100)
 
             while True:
-                print("Payment due: ", due_pay, " dollars and ", due_pay1, " cents")
+                if due_pay == 0:
+                    print("Payment due: ", due_pay1, " cents")
+                else:
+                    print("Payment due: ", due_pay, " dollars and ", due_pay1, " cents")
                 deposit1 = input("Indicate your deposit: ")
                 if deposit1 == 'n':
-                    due_pay1 = round(due_pay1 - 0.05, 2)
+                    due_pay1 = (due_pay1 - 5)
                 elif deposit1 == 'd':
-                    due_pay1 = round(due_pay1 - 0.1, 2)
+                    due_pay1 = (due_pay1 - 10)
                 elif deposit1 == 'q':
-                    due_pay1 = round(due_pay1 - 0.25, 2)
+                    due_pay1 = (due_pay1 - 25)
                 elif deposit1 == 'o':
                     due_pay = due_pay - 1
                 elif deposit1 == 'f':
